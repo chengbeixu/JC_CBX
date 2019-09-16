0d35f6b0b0a40f9773b1a0f72b7f7a819d9b8520
chengbeixu <1494775907@qq.com>
2019-09-16 14:22:02 +0800

    唉

--- 坦克大战/src/tanke/dazhan/YouXi.java
+++ 坦克大战/src/tanke/dazhan/YouXi.java
@@ -43,7 +43,7 @@ public class YouXi extends Frame {
 	}
 	
 	public void paint(Graphics g) {
-		System.out.println("123456789000");
+		System.out.println("123456789000111");
 		g.fillRect(100, 100, 50, 50);
 	}
 
